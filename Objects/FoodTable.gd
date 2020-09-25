4fb1a59dbd740264e91909677355036e683b4885
magic <>
2020-09-25 18:41:34 +0200

    you can win

--- Objects/FoodTable.gd
+++ Objects/FoodTable.gd
@@ -10,7 +10,7 @@ func activate():
 	if player.food_count > PlayerData.food_delivered:
 		deliver_food(PlayerData.food_delivered - player.food_count)
 	
-	if player.food_count > 90:
+	if player.food_count >= 80:
 		king_happy()
 	else:
 		king_mad()
@@ -19,8 +19,14 @@ func deliver_food(amount):
 	pass
 
 func king_happy():
+	get_parent().get_node("SpeechBubble").win()
+	$WinTimer.start()
 	pass
 
 func king_mad():
 	animationPlayer.play("open_door")
 	pass
+
+
+func _on_WinTimer_timeout():
+	get_tree().change_scene("res://GUI/EndScreen.tscn")
